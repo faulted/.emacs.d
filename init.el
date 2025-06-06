eded125b81d27bdf1f229344f858740bca5730c0
Chris <cobb.chris.l@gmail.com>
2025-06-05 22:00:18 -0400

    Inital Emacs config commit

---
+++ init.el
@@ -0,0 +1,74 @@
+;; Initialize package sources
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")
+			 ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)  ; Disable visible scrollbar
+(tool-bar-mode -1)    ; Disable the toolbar
+(menu-bar-mode -1)    ; Disable the menu bar
+(set-fringe-mode 10)  ; Give some room on the sides
+
+(which-key-mode)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (setq doom-themes-enable-bold t
+	doom-themes-enable-italic t)
+  (load-theme 'doom-nord t))
+
+(doom-themes-visual-bell-config)
+(doom-themes-org-config)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom
+  ((doom-modeline-height 30)
+   (doom-modeline-total-line-number t)))
+
+;; Put backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/backups/" t)
+
+(global-display-line-numbers-mode t)
+
+(use-package org-superstar
+  :hook (org-mode))
+
+(use-package org-auto-tangle
+  :defer t
+  :hook (org-mode . org-auto-tangle-mode))
+
+(require 'org-tempo)
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+
+(use-package counsel)
+
+(use-package ivy
+    :config
+    (ivy-mode 1)
+    (counsel-mode 1)
+    (setopt ivy-use-virtual-buffers t)
+    (setopt ivy-count-format "(%d/%d) "))
+
+  (use-package swiper
+    :bind
+    (("M-C-s" . swiper)))
+
+(use-package magit)
